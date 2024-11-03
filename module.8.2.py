41ec845bb30231aff96411ba0a307dc0bc3ca09a
Daniil_Tchetyra <defender.0599@gmail.com>
2024-11-03 20:35:54 +0300

    Add files via upload

---
+++ module.8.2.py
@@ -0,0 +1,31 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1
+
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        if not isinstance(numbers, (list, tuple)):
+            raise TypeError
+        total, incorrect_data = personal_sum(numbers)
+        if len(numbers) - incorrect_data == 0:
+            return 0
+        return total / (len(numbers) - incorrect_data)
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Смешанные данные
+print(f'Результат 3: {calculate_average(567)}')  # Не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Корректные данные
